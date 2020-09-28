e06487add0561ed25b9d1faea5c2e525360c2bd9
Shaivye <63388959+Shaivye@users.noreply.github.com>
2020-09-29 02:55:42 +0530

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  title = "MY TODO'S";
+  todos=[{
+    label: 'Complete lab assignment',
+   
+  },
+{label: 'Buy groceries',
+
+},
+{label: 'Prepare for exam',
+
+},
+{label: 'Clean the house',
+
+},
+
+];
+public count=4;
+addTodo(newTodoLabel)
+{
+  var newTodo={
+    label: newTodoLabel,
+  
+    
+  };
+  this.count++;
+  this.todos.push(newTodo);
 }
+deleteTodo(todo){
+  this.todos = this.todos.filter( t => t.label !== todo.label),
+  this.count--;
+}
+
+}
+
